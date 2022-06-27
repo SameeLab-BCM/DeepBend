bea646666a405b97e83209745a28680dcd49f0a8
Samin Rahman Khan <saminrahmankhan97@gmail.com>
2022-06-28 00:25:40 +0600

    Format and Cross-validation

--- src/models/model35.py
+++ src/models/model35.py
@@ -10,23 +10,25 @@ from .utils import get_loss
 from .custom_regularizers import LRange, LVariance, L1Variance, LEntropy
 
 import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 class nn_model:
     def __init__(
-            self,
-            dim_num=(50, 4),
-            filters_conv_0=128,
-            filters_conv_1=32,
-            alpha=75.0,
-            beta=1/75.0,
-            kernel_size_0=8,
-            kernel_size_1=48,
-            regularizer_2='lrange',
-            loss_func='coeff_determination',
-            optimizer='Adam',
-            hyperparameters=None):
+        self,
+        dim_num=(50, 4),
+        filters_conv_0=128,
+        filters_conv_1=32,
+        alpha=75.0,
+        beta=1 / 75.0,
+        kernel_size_0=8,
+        kernel_size_1=48,
+        regularizer_2="lrange",
+        loss_func="coeff_determination",
+        optimizer="Adam",
+        hyperparameters=None,
+    ):
         """initialize basic parameters"""
 
         if hyperparameters:
@@ -39,8 +41,12 @@ class nn_model:
             self.optimizer = hyperparameters.get("optimizer", optimizer)
             self.alpha = hyperparameters.get("alpha", alpha)
             self.beta = hyperparameters.get("beta", beta)
-            self.multinomial_bkg = [hyperparameters.get("A", 0.25), hyperparameters.get("C", 0.25),
-                                    hyperparameters.get("G", 0.25), hyperparameters.get("T", 0.25)]
+            self.multinomial_bkg = [
+                hyperparameters.get("A", 0.25),
+                hyperparameters.get("C", 0.25),
+                hyperparameters.get("G", 0.25),
+                hyperparameters.get("T", 0.25),
+            ]
         else:
             self.filters_conv_0 = filters_conv_0
             self.filters_conv_1 = filters_conv_1
@@ -59,12 +65,20 @@ class nn_model:
         # building model
         # To build this model with the functional API,
         # you would start by creating an input node:
-        forward = keras.Input(shape=self.dim_num, name='forward')
-        reverse = keras.Input(shape=self.dim_num, name='reverse')
+        forward = keras.Input(shape=self.dim_num, name="forward")
+        reverse = keras.Input(shape=self.dim_num, name="reverse")
 
-        first_layer_1 = MultinomialConvolutionLayer(alpha=self.alpha, beta=self.beta, filters=self.filters_conv_0,
-                                        kernel_size=self.kernel_size_0, background=self.multinomial_bkg,
-                                        strides=1, data_format='channels_last', use_bias=True, padding='same')
+        first_layer_1 = MultinomialConvolutionLayer(
+            alpha=self.alpha,
+            beta=self.beta,
+            filters=self.filters_conv_0,
+            kernel_size=self.kernel_size_0,
+            background=self.multinomial_bkg,
+            strides=1,
+            data_format="channels_last",
+            use_bias=True,
+            padding="same",
+        )
 
         fw_1 = first_layer_1(forward)
         rc_1 = first_layer_1(reverse)
@@ -76,11 +90,11 @@ class nn_model:
             filters=self.filters_conv_1,
             kernel_size=self.kernel_size_1,
             strides=1,
-            data_format='channels_last',
+            data_format="channels_last",
             use_bias=True,
-            kernel_initializer='normal',
+            kernel_initializer="normal",
             kernel_regularizer=regularizers.l2(0.0005),
-            padding='same',
+            padding="same",
         )
 
         fw_out_2 = conv_2(fw_relu_1)
@@ -89,27 +103,27 @@ class nn_model:
         out_2 = Maximum()([fw_out_2, rc_out_2])
         relu_2 = ReLU()(out_2)
 
-        if self.regularizer_2 == 'l1':
-            reg = regularizers.l1(.0005)
-        elif self.regularizer_2 == 'lrange':
-            reg = LRange(lrange=relu_2.shape[1], limit=.05)
-        elif self.regularizer_2 == 'lvariance':
+        if self.regularizer_2 == "l1":
+            reg = regularizers.l1(0.0005)
+        elif self.regularizer_2 == "lrange":
+            reg = LRange(lrange=relu_2.shape[1], limit=0.05)
+        elif self.regularizer_2 == "lvariance":
             reg = LVariance(lvariance=relu_2.shape[1])
-        elif self.regularizer_2 == 'l1variance':
-            reg = L1Variance(l1=.0005, lvariance=relu_2.shape[1])
-        elif self.regularizer_2 == 'l2':
-            reg = regularizers.l2(.001)
+        elif self.regularizer_2 == "l1variance":
+            reg = L1Variance(l1=0.0005, lvariance=relu_2.shape[1])
+        elif self.regularizer_2 == "l2":
+            reg = regularizers.l2(0.001)
         else:
             reg = None
 
         output_conv = Conv1D(
             filters=1,
             kernel_size=relu_2.shape[1],
-            data_format='channels_last',
+            data_format="channels_last",
             use_bias=True,
-            kernel_initializer='normal',
+            kernel_initializer="normal",
             kernel_regularizer=reg,
-            activation='linear'
+            activation="linear",
         )
 
         outputs = Flatten()(output_conv(relu_2))
@@ -118,7 +132,10 @@ class nn_model:
 
         model.summary()
 
-        model.compile(loss=get_loss(self.loss_func), optimizer=self.optimizer,
-                      metrics=[coeff_determination, spearman_fn])
+        model.compile(
+            loss=get_loss(self.loss_func),
+            optimizer=self.optimizer,
+            metrics=[coeff_determination, spearman_fn],
+        )
 
         return model
